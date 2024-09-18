3d9c4f9d367a8515b3084af260a2dbc19aa37c73
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2024-09-18 19:31:00 -0300

    other adds

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,12 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
-    '@': path.resolve(__dirname, './src')
-    }
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
