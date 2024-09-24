ae53043b5665f40ad7d4533bd4433ef38cf6f575
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2024-09-24 19:25:13 -0300

    Feat: Avatar List Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,33 @@
-import { CardComponent, Header } from "@/components"
+import { AvatarList, CardComponent, Header } from "@/components"
 import { Container } from "@mui/material"
+import { currencyConverter } from "@/utils"
 
 function Home() {
+
+  const mockListData = [
+    {
+      avatar: '/Avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(12345.545),
+    },
+    {
+      avatar: '/Avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(3495.55),
+    },
+    {
+      avatar: '/Avatar.svg',
+      name: 'Nome Sobrenome 3',
+      subtitle: currencyConverter(12555.45),
+    },
+  ]
+
   return (
     <>
       <Header />
       <Container maxWidth="lg">
         <CardComponent>CARD</CardComponent>
+        <CardComponent>< AvatarList listsData={mockListData} /></CardComponent>
       </Container>
     </>
   )
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from './theme'
 export * from './formComponents'
+export * from './avatarLists'
 export * from './typographies'
